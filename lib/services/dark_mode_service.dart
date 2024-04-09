19c5d3a97d61dd712c31f70608daa39143dbc195
Guilherme Pinto Lima de Almeida <dev@iugmali.com>
2024-04-09 14:49:05 -0300

    enable dark mode only if dark mode is defined

--- lib/services/dark_mode_service.dart
+++ lib/services/dark_mode_service.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class DarkModeService extends ChangeNotifier {
-  bool _darkMode = true;
+  bool _darkMode = SchedulerBinding.instance.platformDispatcher.platformBrightness == Brightness.dark;
 
   bool get darkMode => _darkMode;
 
